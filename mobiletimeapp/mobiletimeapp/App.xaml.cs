dc8f45c63fb6afc8043a0a644fbfb8cef958e302
Joren Vansteenkiste <vansteenkiste.joren@telenet.be>
2024-02-18 15:57:18 +0100

    basic xamarin app added

---
+++ mobiletimeapp/mobiletimeapp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using mobiletimeapp.Services;
+using mobiletimeapp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobiletimeapp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ mobiletimeapp/mobiletimeapp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using mobiletimeapp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace mobiletimeapp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
