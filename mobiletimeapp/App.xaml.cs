53ce0a37f404ef822955d969283d8677aa331021
Joren Vansteenkiste <vansteenkiste.joren@telenet.be>
2024-02-19 20:24:59 +0100

    replaced Xamarin project with .net maui (xamarin will not be supported from 1 may 2024)

---
+++ mobiletimeapp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mobiletimeapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
