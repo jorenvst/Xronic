d0647d540669fbffa85a40232477c9721fba41c0
Joren Vansteenkiste <joren.vansteenkiste@ugent.be>
2024-08-02 16:58:03 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:xronic/pages/my_home_page.dart";
 
 void main() {
   runApp(const MyApp());
@@ -10,36 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Xronic',
+      title: "Xronic",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Xronic'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-      ),
+      home: const XronicHomePage(title: "Xronic"),
     );
   }
 }
---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class XronicHomePage extends StatefulWidget {
+  const XronicHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<XronicHomePage> createState() => _XronicHomePageState();
+}
+
+class _XronicHomePageState extends State<XronicHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
